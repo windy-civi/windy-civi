9f5eaa7c7a4dcbea52518a1f01869b5ea87a75dc
Sartaj <sartaj@sartaj.me>
2024-08-20 02:35:06 -0500

    Change Release URL on scraper (#4)

--- scraper/api/api.ts
+++ scraper/api/api.ts
@@ -2,10 +2,10 @@ import { locales, Locales } from "./types";
 
 export const civiLegislationApi = {
   getLegislationDataUrl: (locale: Locales): string => {
-    return `https://github.com/civi-social/civi-legislation-data/releases/download/nightly/${locale}.legislation.json`;
+    return `https://github.com/sartaj/windy-civi/releases/download/nightly/${locale}.legislation.json`;
   },
   getGptLegislationUrl: (locale: Locales): string => {
-    return `https://github.com/civi-social/civi-legislation-data/releases/download/nightly/${locale}.legislation.gpt.json`;
+    return `https://github.com/sartaj/windy-civi/releases/download/nightly/${locale}.legislation.gpt.json`;
   },
   locales,
 };
