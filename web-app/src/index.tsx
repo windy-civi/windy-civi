825a45ede11e360c5428f45fe5ca7e360f312ca0
Fiona Tang <fionatang@berkeley.edu>
2024-08-29 01:51:39 +0000

    react router route

---
+++ web-app/src/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider, } from "react-router-dom";
+import './index.css';
+import { RemixApp } from './RemixApp';
+import reportWebVitals from './reportWebVitals';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world</div>
+  }
+]);
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+    <React.StrictMode>
+      <RouterProvider router={router} />
+      <RemixApp />
+    </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
