7aa2a907c36704f88b3f4f23b8a83253cef44e0d
Sartaj <sartaj@sartaj.me>
2024-09-11 01:04:05 -0500

    Make Base URL Relative (#11)

--- web-app/vite.config.js
+++ web-app/vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "",
   plugins: [
     react(),
     tsconfigPaths(),
