9e714383b2e2e9606d3adb68150d2192d1820420
Andrew Dauphinais <andrew.dauphinais@konverse.com>
2024-10-28 15:39:00 -0500

    Created basic Expo push server with Express. Created useGetPushToken hook in native-app to get expo push token from device.

---
+++ native-app/app/hooks/useGetPushToken.ts
@@ -0,0 +1,39 @@
+import {
+  getPermissionsAsync,
+  requestPermissionsAsync,
+  // getDevicePushTokenAsync,
+  getExpoPushTokenAsync,
+} from "expo-notifications";
+
+// Will need to replace this when we build for production
+const projectId = process.env.EXPO_PUBLIC_API_URL;
+
+export const useGetPushToken = () => {
+  async function getPushToken() {
+    try {
+      // Request permissions for push notifications
+      const { status: existingStatus } = await getPermissionsAsync();
+      let finalStatus = existingStatus;
+
+      if (existingStatus !== "granted") {
+        const { status } = await requestPermissionsAsync();
+        finalStatus = status;
+      }
+
+      if (finalStatus !== "granted") {
+        console.log("Failed to get push token for push notification!");
+        return;
+      }
+
+      // Get the push token
+      const token = await getExpoPushTokenAsync({ projectId });
+
+      console.log("Push token:", token);
+      return token;
+    } catch (error) {
+      console.log("Error getting push token:", error);
+    }
+  }
+
+  return { getPushToken };
+};
