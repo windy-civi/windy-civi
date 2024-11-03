95a0575ef47bfd9c1066e52d81e0ed3b0f35d5f2
Sartaj <sartaj@sartaj.me>
2024-11-03 17:42:17 -0600

    Update api.ts to point to windy-civi/windy-civi git repo

--- scraper/api/api.ts
+++ scraper/api/api.ts
@@ -3,10 +3,10 @@ import { Locales } from "../../domain/types";
 
 export const civiLegislationApi = {
   getLegislationDataUrl: (locale: Locales): string => {
-    return `https://github.com/sartaj/windy-civi/releases/download/nightly/${locale}.legislation.json`;
+    return `https://github.com/windy-civi/windy-civi/releases/download/nightly/${locale}.legislation.json`;
   },
   getGptLegislationUrl: (locale: Locales): string => {
-    return `https://github.com/sartaj/windy-civi/releases/download/nightly/${locale}.legislation.gpt.json`;
+    return `https://github.com/windy-civi/windy-civi/releases/download/nightly/${locale}.legislation.gpt.json`;
   },
   locales: SupportedLocale,
 };
