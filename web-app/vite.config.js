afface692e67cb3928cd8321f48a24b81f5bb585
Sartaj <sartaj@sartaj.me>
2024-11-04 01:15:17 +0000

    update

--- web-app/vite.config.js
+++ web-app/vite.config.js
@@ -4,9 +4,6 @@ import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 // https://vitejs.dev/config/
 export default defineConfig({
-  // This base is based on where it is deployed currently on github pages.
-  // Needs to be changed if deployed elsewhere. (e.g custom domain)
-  base: "/windy-civi/",
   plugins: [
     react(),
     tsconfigPaths(),
