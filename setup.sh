0baa3f0b7dfbe036a35e59664a4afc970b233fc2
Sartaj <sartaj@sartaj.me>
2024-11-30 20:03:43 -0600

    update

--- setup.sh
+++ setup.sh
@@ -1,16 +1,38 @@
-echo "🏗️ Installing Domain"
+#!/bin/bash
+
+log "🏗️ Installing Domain"
 cd ./domain
-echo "🏗️ Installing windy-civi-storage Storage CLI"
+log "🏗️ Installing windy-civi-storage Storage CLI"
 cd ../storage
 npm i
-echo "🏗️ Installing Scraper"
+log "🏗️ Installing Scraper"
 cd ../scraper
 npm i
-echo "🏗️ Installing Web App"
+log "🏗️ Installing Web App"
 cd ../web-app
 npm i
-echo "🏗️ Installing Expo React Native"
-echo "🏗️ Warning: For Codespaces, The App Will Only Work With Expo"
-cd ../native
+log "🏗️ Installing Expo React Native \n 🏗️ Warning: For Codespaces, The App Will Only Work With Expo"
+cd ../native-app
 npm i
+
+
+# Function to output banner-style logs
+log() {
+    local message="$1"
+    local padding=4 # Spaces around the message inside the box
+    local border_char="*"
+    local inner_width=$(( ${#message} + (padding * 2) ))
+
+    # Create the border
+    local border=$(printf "%${inner_width}s" | tr " " "$border_char")
+
+    echo
+    echo "$border"
+    printf "%s%s%s\n" "$border_char" "$(printf "%-${inner_width}s" | sed "s/ / /g")" "$border_char"
+    printf "%s%s%s\n" "$border_char" "$(printf "%-${padding}s$message%${padding}s" | cut -c1-${inner_width})" "$border_char"
+    printf "%s%s%s\n" "$border_char" "$(printf "%-${inner_width}s" | sed "s/ / /g")" "$border_char"
+    echo "$border"
+    echo
+}
+
 
