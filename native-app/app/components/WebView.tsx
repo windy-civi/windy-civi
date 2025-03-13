072cc163f9174dac56c386b81ef69b4005a0255a
Sartaj <sartaj@sartaj.me>
2025-03-12 19:51:27 -0500

    initial types

--- native-app/app/components/WebView.tsx
+++ native-app/app/components/WebView.tsx
@@ -4,7 +4,14 @@ import {
   WebViewMessageEvent,
 } from "react-native-webview";
 import { Linking } from "react-native";
-import { onUserPreferences } from "../helpers/native-web-bridge";
+import {
+  parseEvent,
+  USER_PREFERENCES_CHANGED,
+  REQUEST_NATIVE_NOTIFICATION_PERMISSIONS,
+  NATIVE_NOTIFICATION_STATUS_REQUESTED,
+  Events,
+  UserPreferencesChangedEvent,
+} from "@windy-civi/domain/native-web-bridge/native-bridge";
 import { useStorage } from "../helpers/hooks/useStorage";
 
 export default function WebView() {
@@ -12,12 +19,35 @@ export default function WebView() {
 
   const handleMessage = useCallback(
     (event: WebViewMessageEvent) => {
-      onUserPreferences((userPreferences) => {
-        storeData({
-          key: "userPreferences",
-          value: JSON.stringify(userPreferences),
-        });
-      }, event.nativeEvent.data);
+      const e = parseEvent(event.nativeEvent.data);
+      if (!e) {
+        throw new Error("Invalid event");
+        // TODO: handle error
+      }
+
+      // Define side effects. Type should force exhaustive checking of events
+      const sideEffects = <T extends Events>(
+        type: T["type"],
+        payload: T["payload"]
+      ): T["type"] => {
+        switch (type) {
+          case USER_PREFERENCES_CHANGED:
+            storeData({
+              key: "userPreferences",
+              value: JSON.stringify(payload),
+            });
+            return USER_PREFERENCES_CHANGED;
+          case REQUEST_NATIVE_NOTIFICATION_PERMISSIONS:
+            return REQUEST_NATIVE_NOTIFICATION_PERMISSIONS;
+          case NATIVE_NOTIFICATION_STATUS_REQUESTED:
+            return NATIVE_NOTIFICATION_STATUS_REQUESTED;
+          default:
+            throw new Error(`Unknown event type: ${type}`);
+        }
+      };
+
+      // Run the side effect
+      sideEffects(e.type, e.payload);
     },
     [storeData]
   );
