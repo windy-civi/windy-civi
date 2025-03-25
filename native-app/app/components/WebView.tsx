ee89fba31661fd51c82ee7dc65e9b92437b10052
Andrew Dauphinais <63437734+adaup1@users.noreply.github.com>
2025-03-25 18:52:37 -0500

    Handle events on native based on web messages

--- native-app/app/components/WebView.tsx
+++ native-app/app/components/WebView.tsx
@@ -1,55 +1,8 @@
-import { useCallback } from "react";
-import {
-  WebView as NativeWebView,
-  WebViewMessageEvent,
-} from "react-native-webview";
+import { WebView as NativeWebView } from "react-native-webview";
 import { Linking } from "react-native";
-import {
-  parseEvent,
-  USER_PREFERENCES_CHANGED,
-  REQUEST_NATIVE_NOTIFICATION_PERMISSIONS,
-  NATIVE_NOTIFICATION_STATUS_REQUESTED,
-  Events,
-} from "@windy-civi/domain/native-web-bridge/native-bridge";
-import { useStorage } from "../helpers/hooks/useStorage";
-
+import { useHandleWebBridgeMessage } from "../helpers/hooks/useHandleWebBridgeMessage";
 export default function WebView() {
-  const { storeData } = useStorage();
-
-  const handleMessage = useCallback(
-    (event: WebViewMessageEvent) => {
-      const e = parseEvent(event.nativeEvent.data);
-      if (!e) {
-        throw new Error("Invalid event");
-        // TODO: handle error
-      }
-
-      // Define side effects. Type should force exhaustive checking of events
-      const sideEffects = (
-        type: Events["type"],
-        payload: Events["payload"]
-      ) => {
-        switch (type) {
-          case USER_PREFERENCES_CHANGED:
-            storeData({
-              key: "userPreferences",
-              value: JSON.stringify(payload),
-            });
-            return USER_PREFERENCES_CHANGED;
-          case REQUEST_NATIVE_NOTIFICATION_PERMISSIONS:
-            return REQUEST_NATIVE_NOTIFICATION_PERMISSIONS;
-          case NATIVE_NOTIFICATION_STATUS_REQUESTED:
-            return NATIVE_NOTIFICATION_STATUS_REQUESTED;
-          default:
-            throw new Error(`Unknown event type: ${type}`);
-        }
-      };
-
-      // Run the side effect
-      sideEffects(e.type as Events["type"], e.payload);
-    },
-    [storeData]
-  );
+  const { handleWebBridgeMessage } = useHandleWebBridgeMessage();
 
   return (
     <NativeWebView
@@ -57,7 +10,7 @@ export default function WebView() {
       bounces={false}
       overScrollMode="never"
       pullToRefreshEnabled={false}
-      onMessage={handleMessage}
+      onMessage={handleWebBridgeMessage}
       onShouldStartLoadWithRequest={(event) => {
         if (event.navigationType === "click" && event.url) {
           Linking.openURL(event.url);
